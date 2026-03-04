dd3e575fac8a8c5578daef8c5a4eff71a6654288
Robinson69430 <moncharmont.robinson@icloud.com>
2026-03-04 15:40:45 +0100

    finis

--- views/index.php
+++ views/index.php
@@ -9,90 +9,194 @@ $dao3 = new DAO_Ressource();
 $applications = $dao->getTopApplications();
 $conso = $dao2->getConso();
 $ressou = $dao3->getRessource();
-
-
 ?>
 
 <!DOCTYPE html>
-<html>
-  <head>
-    <link rel="stylesheet" type="text/css" href="../css/style.css"/>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-  </head>
-  <body>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" type="text/css" href="../css/style.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+</head>
+
+<body>
+
     <div class="tab">
-      <button class="tablinks" onclick="openTab(event, 'Tab1')">Application</button>
-      <button class="tablinks" onclick="openTab(event, 'Tab2')">Consomation</button>
-      <button class="tablinks" onclick="openTab(event, 'Tab3')">Ressource</button>
+        <div>
+            <button class="tablinks" onclick="openTab(event, 'Tab1')">Application</button>
+            <button class="tablinks" onclick="openTab(event, 'Tab2')">Consommation</button>
+            <button class="tablinks" onclick="openTab(event, 'Tab3')">Ressource</button>
+        </div>
+        <div class="img">
+            <img src="../logoBusinessSchool.png" alt="">
+        </div>
     </div>
+
+    <!-- TAB 1 : Applications -->
     <div id="Tab1" class="tabcontent">
-<table class="table">
-    <tr>
-        <th scope="col">Application</th>
-        <th scope="col">Total (unités cumulées)</th>
-    </tr>
-
-    <?php if (!empty($applications)) : ?>
-        <?php foreach ($applications as $row) : ?>
-            <tr scope="row">
-                <td scope="col"><?= htmlspecialchars($row['nom']) ?></td>
-                <td scope="col"><?= htmlspecialchars($row['total']) ?></td>
-            </tr>
-        <?php endforeach; ?>
-    <?php else : ?>
-        <tr>
-            <td colspan="2">Aucune donnée</td>
-        </tr>
-    <?php endif; ?>
-</table>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Application</th>
+                    <th scope="col">Total (unités cumulées)</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php if (!empty($applications)): ?>
+                    <?php foreach ($applications as $row): ?>
+                        <tr>
+                            <td><?= htmlspecialchars($row['nom']) ?></td>
+                            <td><?= htmlspecialchars($row['total']) ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                <?php else: ?>
+                    <tr>
+                        <td colspan="2">Aucune donnée</td>
+                    </tr>
+                <?php endif; ?>
+            </tbody>
+        </table>
+        <div class="chart-container">
+            <canvas id="chartApplications"></canvas>
+        </div>
     </div>
 
-<div id="Tab2" class="tabcontent">
-<table class="table">
-    <tr>
-        <th scope="col">Application</th>
-        <th scope="col">Total (unités cumulées)</th>
-    </tr>
-
-    <?php if (!empty($conso)) : ?>
-        <?php foreach ($conso as $row) : ?>
-            <tr>
-                <td scope="col"><?= htmlspecialchars($row['mois']) ?></td>
-                <td scope="col"><?= htmlspecialchars($row['total']) ?></td>
-            </tr>
-        <?php endforeach; ?>
-    <?php else : ?>
-        <tr>
-            <td colspan="2">Aucune donnée</td>
-        </tr>
-    <?php endif; ?>
-</table>
+    <!-- TAB 2 : Consommation -->
+    <div id="Tab2" class="tabcontent">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Mois</th>
+                    <th scope="col">Total (unités cumulées)</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php if (!empty($conso)): ?>
+                    <?php foreach ($conso as $row): ?>
+                        <tr>
+                            <td><?= htmlspecialchars($row['mois']) ?></td>
+                            <td><?= htmlspecialchars($row['total']) ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                <?php else: ?>
+                    <tr>
+                        <td colspan="2">Aucune donnée</td>
+                    </tr>
+                <?php endif; ?>
+            </tbody>
+        </table>
+        <div class="chart-container">
+            <canvas id="chartConso"></canvas>
+        </div>
     </div>
 
+    <!-- TAB 3 : Ressource -->
     <div id="Tab3" class="tabcontent">
-<table class="table">
-    <tr>
-        <th scope="col">Application</th>
-        <th scope="col">Total (unités cumulées)</th>
-    </tr>
-
-    <?php if (!empty($ressou)) : ?>
-        <?php foreach ($ressou as $row) : ?>
-            <tr>
-                <td scope="col"><?= htmlspecialchars($row['mois']) ?></td>
-                <td scope="col"><?= htmlspecialchars($row['Stockage']) ?></td>
-                <td scope="col"><?= htmlspecialchars($row['Réseau']) ?></td>
-            </tr>
-        <?php endforeach; ?>
-    <?php else : ?>
-        <tr>
-            <td colspan="2">Aucune donnée</td>
-        </tr>
-    <?php endif; ?>
-</table>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Mois</th>
+                    <th scope="col">Stockage</th>
+                    <th scope="col">Réseau</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php if (!empty($ressou)): ?>
+                    <?php foreach ($ressou as $row): ?>
+                        <tr>
+                            <td><?= htmlspecialchars($row['mois']) ?></td>
+                            <td><?= htmlspecialchars($row['Stockage']) ?></td>
+                            <td><?= htmlspecialchars($row['Réseau']) ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                <?php else: ?>
+                    <tr>
+                        <td colspan="3">Aucune donnée</td>
+                    </tr>
+                <?php endif; ?>
+            </tbody>
+        </table>
+        <div class="chart-container">
+            <canvas id="chartRessource"></canvas>
+        </div>
     </div>
 
     <script src="../js/script.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-  </body>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+        crossorigin="anonymous"></script>
+
+    <script>
+        // Chart Applications
+        const appLabels = <?= json_encode(array_column($applications, 'nom')) ?>;
+        const appData = <?= json_encode(array_column($applications, 'total')) ?>;
+
+        new Chart(document.getElementById('chartApplications'), {
+            type: 'bar',
+            data: {
+                labels: appLabels,
+                datasets: [{
+                    label: 'Total par application',
+                    data: appData,
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                responsive: true,
+                plugins: { legend: { display: false } },
+                scales: { y: { beginAtZero: true } }
+            }
+        });
+
+        // Chart Consommation
+        const consoLabels = <?= json_encode(array_column($conso, 'mois')) ?>;
+        const consoData = <?= json_encode(array_column($conso, 'total')) ?>;
+
+        new Chart(document.getElementById('chartConso'), {
+            type: 'line',
+            data: {
+                labels: consoLabels,
+                datasets: [{
+                    label: 'Consommation mensuelle',
+                    data: consoData,
+                    tension: 0.3,
+                    fill: false
+                }]
+            },
+            options: { responsive: true }
+        });
+
+        // Chart Ressource
+        const resLabels = <?= json_encode(array_column($ressou, 'mois')) ?>;
+        const stockageData = <?= json_encode(array_column($ressou, 'Stockage')) ?>;
+        const reseauData = <?= json_encode(array_column($ressou, 'Réseau')) ?>;
+
+        new Chart(document.getElementById('chartRessource'), {
+            type: 'line',
+            data: {
+                labels: resLabels,
+                datasets: [
+                    {
+                        label: 'Stockage',
+                        data: stockageData,
+                        tension: 0.3
+                    },
+                    {
+                        label: 'Réseau',
+                        data: reseauData,
+                        tension: 0.3
+                    }
+                ]
+            },
+            options: { responsive: true }
+        });
+    </script>
+
+</body>
+
 </html>
